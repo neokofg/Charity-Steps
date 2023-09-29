05100d0b488dba3ba76e0bf1bd9835a53738b6a4
neoko <33611354+neokofg@users.noreply.github.com>
2023-09-30 01:58:02 +0300

    fix/db-connection

--- backend/config/database.php
+++ backend/config/database.php
@@ -75,10 +75,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'require',
-            'options' => [
-                "endpoint" => env('DB_ENDPOINT', null)
-            ],
+            'sslmode' => 'verify-ca',
         ],
 
         'sqlsrv' => [

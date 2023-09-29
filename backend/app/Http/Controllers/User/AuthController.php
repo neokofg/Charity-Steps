9cda700b774cca1847e20e25f81c1c613ad1624c
neoko <33611354+neokofg@users.noreply.github.com>
2023-09-29 18:09:46 +0300

    initial commit

---
+++ backend/app/Http/Controllers/User/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Services\UserService;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function __construct(private UserService $userService)
+    {
+
+    }
+}

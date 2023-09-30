e4c2f64789dc38bc1c43542926be0479c09bff42
neoko <33611354+neokofg@users.noreply.github.com>
2023-09-30 10:36:53 +0300

    feat/update-avatar-s3

--- backend/app/Http/Controllers/User/UserController.php
+++ backend/app/Http/Controllers/User/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\EmailApproveRequest;
 use App\Http\Requests\EmailUpdateRequest;
+use App\Http\Requests\UpdateAvatarRequest;
 use App\Http\Requests\UserUpdateRequest;
 use App\Services\UserService;
 use Illuminate\Http\JsonResponse;
@@ -16,6 +17,16 @@ class UserController extends Controller
     public function __construct(private UserService $userService)
     {
 
+    }
+    public function get_user(): JsonResponse
+    {
+        $user = Auth::user();
+        $response = $this->userService->get_user($user);
+        if($response) {
+            return response()->json(["message" => "Пользователь получен!", "status" => true, "user" => $response], Response::HTTP_OK);
+        } else {
+            return response()->json(["message" => "Произошла ошибка!", "status" => false], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     public function update_user(UserUpdateRequest $request): JsonResponse
@@ -46,6 +57,18 @@ class UserController extends Controller
         $response = $this->userService->user_update_email_approve($request->all(), $user);
         if($response) {
             return response()->json(["message" => "Почта успешно обновлена!", "status" => true], Response::HTTP_OK);
+        } else {
+            return response()->json(["message" => "Произошла ошибка!", "status" => false], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function update_avatar(UpdateAvatarRequest $request): JsonResponse
+    {
+        $user = Auth::user();
+        $file = $request->file('file');
+        $response = $this->userService->update_avatar($file, $user);
+        if($response) {
+            return response()->json(["message" => "Аватар успешно обновлен!", "status" => true], Response::HTTP_OK);
         } else {
             return response()->json(["message" => "Произошла ошибка!", "status" => false], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
